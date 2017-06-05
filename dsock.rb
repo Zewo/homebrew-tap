e9899d4f6310fb506329b696f3817f0f2b55622a
David Ask <david@formbound.com>
2017-06-05 21:05:27 +0200

    It starts

---
+++ dsock.rb
@@ -0,0 +1,17 @@
+class Dsock < Formula
+  homepage "https://zewo.io"
+  version "0.5-alpha"
+  url "https://github.com/Zewo/dsock.git", tag: version
+  head "https://github.com/Zewo/dsock"
+
+  depends_on "libdill"
+  depends_on "openssl"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "check" if build.with?("test")
+    system "make install"
+  end
+end
---
+++ libdill.rb
@@ -0,0 +1,17 @@
+class Libdill < Formula
+  homepage "https://zewo.io"
+  version "1.6"
+  url "https://github.com/Zewo/libdill.git", tag: version
+  head "https://github.com/Zewo/libdill"
+
+  depends_on "automake"
+  depends_on "libtool"
+
+  def install
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "check" if build.with?("test")
+    system "make install"
+  end
+end
