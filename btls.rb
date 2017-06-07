41c2965fe50dc34aed5f45dd3d385f140f2ab440
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-07 16:42:15 -0300

    add btls

---
+++ btls.rb
@@ -0,0 +1,14 @@
+class Btls < Formula
+  homepage "https://zewo.io"
+  version "0.1.0"
+  url "https://github.com/Zewo/btls.git", tag: version
+  head "https://github.com/Zewo/btls"
+
+  depends_on "libdill"
+  depends_on "libressl"
+
+  def install
+    system "make"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
