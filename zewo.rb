06a7183d9ce6dc3166b4c7051bab0a60450b2b1b
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-08 09:14:11 -0300

    rollback to 0.14.0 zewo version

--- zewo.rb
+++ zewo.rb
@@ -1,6 +1,6 @@
 class Zewo < Formula
   homepage "https://zewo.io"
-  version "0.15.0"
+  version "0.14.0"
   url "https://github.com/Zewo/Zewo.git", tag: version
   head "https://github.com/Zewo/Zewo"
 
